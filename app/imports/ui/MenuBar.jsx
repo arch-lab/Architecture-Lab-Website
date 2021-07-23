eba2e59c28f5f90cda56bf024ab44fc6fe565bf7
Architecture Lab <87790714+arch-lab@users.noreply.github.com>
2021-07-23 13:33:51 -1000

    Changed folder name to app

--- app/imports/ui/MenuBar.jsx
+++ app/imports/ui/MenuBar.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export const MenuBar = () => {
 
   return (
     <div>
       <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">
+          <Navbar.Brand as={Link} to="/">
             <img
               alt=""
               src="https://react-bootstrap.github.io/logo.svg"
@@ -17,6 +24,8 @@ export const MenuBar = () => {
             />{' '}
           React Bootstrap
           </Navbar.Brand>
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/NotHome">Not Home</Nav.Link>
       </Navbar>
     </div>
   );
