0f78741775f99224a9ebaeac2a0cf37a17263d16
Conrad Wolfe <46543798+conradwolfe@users.noreply.github.com>
2021-07-22 23:33:14 -1000

    Added MenuBar
    Added the beginning of a menu using Navbar from React-Bootstrap. Also removed some boiler plate.

--- labwebsite/imports/ui/App.jsx
+++ labwebsite/imports/ui/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
+import { MenuBar } from './MenuBar.jsx';
 import { Info } from './Info.jsx';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
+    <MenuBar/>
     <Info/>
   </div>
 );
