c6fb577424149318afd79f9e6951728474d9b01c
Architecture Lab <87790714+arch-lab@users.noreply.github.com>
2021-07-28 13:02:26 -1000

    More reorganizing

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import NavBar from './components/NavBar.jsx';
+import { Switch, Route } from 'react-router-dom';
+import { NavBar } from './components/NavBar.jsx';
 import { Home } from './pages/Home.jsx';
 import { EquipmentRental } from './pages/EquipmentRental.jsx';
 import { ClassroomReservation } from './pages/ClassroomReservation.jsx';
@@ -9,21 +9,21 @@ import { Footer } from './components/Footer.jsx';
 
 export const App = () => (
   <div>
-    <Router>
       <NavBar/>
-      <Route exact path="/">
-        <Home/>
-      </Route>
-      <Route path="/EquipmentRental">
-        <EquipmentRental/>
-      </Route>
-      <Route path="/ClassroomReservation">
-        <ClassroomReservation/>
-      </Route>
-      <Route path="/SoftwareRequestForms">
-        <SoftwareRequestForms/>
-      </Route>
+      <Switch>
+        <Route path="/EquipmentRental">
+          <EquipmentRental/>
+        </Route>
+        <Route path="/ClassroomReservation">
+          <ClassroomReservation/>
+        </Route>
+        <Route path="/SoftwareRequestForms">
+          <SoftwareRequestForms/>
+        </Route>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+      </Switch>
       <Footer/>
-    </Router>
   </div>
 );
--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -4,35 +4,28 @@ import Nav from 'react-bootstrap/Nav';
 import { Link } from "react-router-dom";
 import { LoginButton } from './LoginButton.jsx'
 
-class NavBar extends React.Component {
-  constructor(props){
-    super(props);
-  }
+export const NavBar = () => {
 
-  render(){
-    return (
-      <div>
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand as={Link} to="/">
-              <img
-                alt=""
-                src="/images/soa-logo.png"
-                height="45"
-              />{' '}
-            </Navbar.Brand>
-            <Nav>
-              <Nav.Link as={ Link } to="/">Home</Nav.Link>
-              <Nav.Link as={ Link } to="/EquipmentRental">Equipment Rental</Nav.Link>
-              <Nav.Link as={ Link } to="/ClassroomReservation">Classroom Reservation</Nav.Link>
-              <Nav.Link as={ Link } to="/SoftwareRequestForms">Software Request Forms</Nav.Link>
-            </Nav>
-            <Navbar.Collapse className="justify-content-end">
-              <LoginButton/>
-            </Navbar.Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand as={Link} to="/">
+          <img
+            alt=""
+            src="/images/soa-logo.png"
+            height="45"
+          />{' '}
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link as={ Link } to="/">Home</Nav.Link>
+          <Nav.Link as={ Link } to="/EquipmentRental">Equipment Rental</Nav.Link>
+          <Nav.Link as={ Link } to="/ClassroomReservation">Classroom Reservation</Nav.Link>
+          <Nav.Link as={ Link } to="/SoftwareRequestForms">Software Request Forms</Nav.Link>
+        </Nav>
+        <Navbar.Collapse className="justify-content-end">
+          <LoginButton/>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
-
-export default NavBar;
