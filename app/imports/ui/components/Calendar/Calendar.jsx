9207c9d306cba9962bc7a2c858e6e31d3e4df362
Architecture Lab <87790714+arch-lab@users.noreply.github.com>
2021-08-16 16:35:32 -1000

    Begin implementing a calendar

---
+++ app/imports/ui/components/Calendar/Calendar.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import styles from './Calendar.css'
+
+export const Calendar = (props) => {
+
+  const title = props.title;
+
+  let startTime = new Date("8/20/2021")
+  let hour = 8;
+  let minute = 30;
+  startTime.setHours(hour, minute)
+
+  let calendarEvent = {
+    "event": "Class",
+    "startTime": startTime,
+    "endTime": ""
+  }
+
+  console.log(Date());
+  console.log();
+  console.log(calendarEvent);
+
+  return(
+    <Container>
+      <h1>{title}</h1>
+      <Row className='row'>
+        <Col className='col'>
+          Monday
+        </Col>
+
+        <Col className='col'>
+          Tuesday
+        </Col>
+
+        <Col className='col'>
+          Wednesday
+        </Col>
+
+        <Col className='col'>
+          Thursday
+        </Col>
+
+        <Col className='col'>
+          Friday
+        </Col>
+
+        <Col className='col'>
+          Saturday
+        </Col>
+
+        <Col className='col'>
+          Sunday
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className='col'>
+          Item 1
+        </Col>
+
+        <Col className='col'>
+          Item 2
+        </Col>
+
+        <Col className='col'>
+          Item 3
+        </Col>
+
+        <Col className='col'>
+          Item 4
+        </Col>
+
+        <Col className='col'>
+          Item 5
+        </Col>
+
+        <Col className='col'>
+          Item 6
+        </Col>
+
+        <Col>
+          Item 7
+        </Col>
+      </Row>
+    </Container>
+  );
+};
